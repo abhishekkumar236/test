54b4bb3ed9be12c909e324b24ca9c8e8ecd75400
abhishekkumar236 <abhishekkumar.ak23610@gmail.com>
2025-01-12 21:18:34 +0530

    dsa

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "hello world" });
+});
+
+app.listen(4000, () => {
+  console.log("server is running on port 3000");
+});
